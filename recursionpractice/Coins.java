81524b25c381b93126b8c1974f53d5e2e2ac3f44
kklike32 <44389276+kklike32@users.noreply.github.com>
2020-08-04 22:44:52 -0700

    Add files via upload

---
+++ recursionpractice/Coins.java
@@ -0,0 +1,31 @@
+package recursionpractice;
+
+import java.util.Scanner;
+
+/**
+ * Ask the user for an amount of cents, and print minimum number of coins needed to equate the amount of cents
+ * 
+ * Note: This should be a recursive function(you can change the parameters if you would like and in fact I recommend
+ * that you do)
+ *
+ */
+
+public class Coins {
+	public static void main(String[] args) {
+		Scanner readInput = new Scanner(System.in);
+		System.out.println("Enter cents: ");
+		
+		System.out.println(minimumCoins(readInput.nextInt()));
+	}
+	
+	private static Integer minimumCoins(Integer c) {
+		/*Write your recursive statement here*/
+	}
+	
+	//test your code by not only running it, but tracing it below
+	
+	/*
+	 * Trace here:
+	 * 
+	 */
+}
